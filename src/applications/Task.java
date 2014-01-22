f293b8057395a0739381d48ebe062340dd8469b9
Todd Malone <malon153@morris.umn.edu>
2014-01-22 17:39:37 -0600

    Extracted EventList made. Changed constructor and methods to public.
    All subclasses have now been extracted to their own class file. 
    
    Actually commited the EventList extraction.

---
+++ src/applications/Task.java
@@ -0,0 +1,15 @@
+package applications;
+
+
+public class Task {
+	// data members
+	public int machine;
+	public int time;
+
+	// constructor
+	public Task(int theMachine, int theTime) {
+		machine = theMachine;
+		time = theTime;
+	}
+}
+
