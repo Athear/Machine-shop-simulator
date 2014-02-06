f6dc2caf67691e0bc99d0829b79ecec4f5e609ca
Todd Malone <malon153@morris.umn.edu>
2014-02-06 11:54:40 -0600

    Changed Machine.changeState to use its new inputs. Also alligned the Machine class file. It was gross.

--- src/applications/Machine.java
+++ src/applications/Machine.java
@@ -18,46 +18,46 @@ public class Machine {
 		finishTime = maxFinishTime;
 	}
 
-    /**
-         * change the state of theMachine
-         * 
-         * @return last job run on this machine
-         * 
-         */
-        
-    //    static Machine getMachineFromArr(int machineAddress){   
-    //        return machine[machineAddress];
-    //    }
-        
-	
-        public Job changeState(int currentTime, int maxTime) {// Task on theMachine has
-                                                    // finished,
-            // schedule next one.
-            Job lastJob;
-            if (this.activeJob == null) {// in idle or change-over
-                                               // state
-                lastJob = null;
-                // wait over, ready for new job
-                if (this.jobQ.isEmpty()){ // no waiting job
-                    finishTime=MachineShopSimulator.getLargeTime();
-                }else {// take job off the queue and work on it
-                    this.activeJob = (Job) this.jobQ.remove();
-                    this.totalWait += MachineShopSimulator.getTimeNow() - this.activeJob.arrivalTime;
-                    this.numTasks++;
-                    int t = this.activeJob.removeNextTask();
-                    finishTime=MachineShopSimulator.getTimeNow() + t;
-                }
-            } else {// task has just finished on machine[theMachine]
-                    // schedule change-over time
-                lastJob = this.activeJob;
-                this.activeJob = null;
-                finishTime=MachineShopSimulator.getTimeNow() + this.changeTime;
-            }
-    
-            return lastJob;
-        }
+	/**
+	 * change the state of theMachine
+	 * 
+	 * @return last job run on this machine
+	 * 
+	 */
 
-    public int nextEventTime() {
-        return finishTime;
-    }
+	//    static Machine getMachineFromArr(int machineAddress){   
+	//        return machine[machineAddress];
+	//    }
+
+
+	public Job changeState(int currentTime, int maxTime) {// Task on theMachine has
+		// finished,
+		// schedule next one.
+		Job lastJob;
+		if (this.activeJob == null) {// in idle or change-over
+			// state
+			lastJob = null;
+			// wait over, ready for new job
+			if (this.jobQ.isEmpty()){ // no waiting job
+				finishTime=maxTime;
+			}else {// take job off the queue and work on it
+				this.activeJob = (Job) this.jobQ.remove();
+				this.totalWait += currentTime - this.activeJob.arrivalTime;
+				this.numTasks++;
+				int t = this.activeJob.removeNextTask();
+				finishTime=currentTime + t;
+			}
+		} else {// task has just finished on machine[theMachine]
+			// schedule change-over time
+			lastJob = this.activeJob;
+			this.activeJob = null;
+			finishTime=currentTime + this.changeTime;
+		}
+
+		return lastJob;
+	}
+
+	public int nextEventTime() {
+		return finishTime;
+	}
 }
