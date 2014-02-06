9dfbceea1bc5be10608d9aebab4667bf8c281951
Todd Malone <malon153@morris.umn.edu>
2014-02-06 11:21:36 -0600

    Removed unused machineIndex field from Task. Removed unused Task constructor.

--- src/applications/Task.java
+++ src/applications/Task.java
@@ -4,18 +4,11 @@ package applications;
 public class Task {
 	// data members
 	public Machine machine;
-	public int machineIndex;
 	public int time;
 
-	// constructor
-	public Task(int theMachine, int theTime) {
-		machineIndex = theMachine;
-		time = theTime;
-	}
-	
+
 	public Task(Machine theMachine, int theTime){
 		machine = theMachine;
-		machineIndex = theMachine.machineIndex;
 		time = theTime;
 	}
 }
